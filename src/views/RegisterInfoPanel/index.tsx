3e83251bfbe39d1abde468f9e2f16c573da348e0
sijin <skd6150@gmail.com>
2022-10-09 22:23:01 +0900

    :sparkles: value validation notification 추가

--- src/views/RegisterInfoPanel/index.tsx
+++ src/views/RegisterInfoPanel/index.tsx
@@ -1,22 +1,41 @@
 import { useState } from "react";
 import { Header, Panel } from "@enact/sandstone/Panels";
 import { useNavigate } from "react-router-dom";
+import LS2Request from "@enact/webos/LS2Request";
 import Input from "@enact/sandstone/Input";
 import Picker from "@enact/sandstone/Picker";
 import Button from "@enact/sandstone/Button";
 import Card from "../../components/Card";
 import Style from "./RegisterInfoPanel.module.css";
 import { InputEvent, PickerEvent } from "../../../types/types";
+import appInfo from "../../../webos-meta/appinfo.json";
 
 const RegisterInfoPanel = () => {
   const navigate = useNavigate();
   const [nickname, setNickname] = useState("");
   const [age, setAge] = useState(0);
   const [gender, setGender] = useState(0);
+  const api = new LS2Request();
+
+  const submit = () => {
+    if (nickname === "") {
+      const option = {
+        service: "com.webos.notification",
+        method: "createToast",
+        parameters: {
+          sourceId: appInfo.id,
+          message: "닉네임을 입력해주세요",
+        },
+      };
+      api.send(option);
+    } else {
+      navigate(`/register/photo/${nickname}/${age}/${gender}`);
+    }
+  };
 
   return (
     <Panel>
-      <Header title="사용자 등록" onClose={() => navigate(-1)} />
+      <Header title="사용자 등록" onClose={() => navigate("/")} />
       <div className={Style.contentWrapper}>
         <Card align_items="flex-start" justify_content="flex_start">
           <h1>사용자 정보</h1>
@@ -48,13 +67,7 @@ const RegisterInfoPanel = () => {
               {["남성", "여성"]}
             </Picker>
           </div>
-          <Button
-            className={Style.next}
-            size={"small"}
-            icon={"arrowsmallright"}
-            iconPosition={"after"}
-            onClick={() => navigate(`/register/photo/${nickname}/${age}/${gender}`)}
-          >
+          <Button className={Style.next} size={"small"} icon={"arrowsmallright"} iconPosition={"after"} onClick={submit}>
             다음
           </Button>
         </Card>
--- src/views/RegisterPhotoPanel/index.tsx
+++ src/views/RegisterPhotoPanel/index.tsx
@@ -2,9 +2,11 @@ import { useState, useRef, useCallback, useEffect } from "react";
 import { Header, Panel } from "@enact/sandstone/Panels";
 import { useNavigate, useMatch } from "react-router-dom";
 import Webcam from "react-webcam";
+import LS2Request from "@enact/webos/LS2Request";
 import Button from "@enact/sandstone/Button";
 import Card from "../../components/Card";
 import Style from "./RegisterPhotoPanel.module.css";
+import appInfo from "../../../webos-meta/appinfo.json";
 
 const RegisterPhotoPanel = () => {
   const navigate = useNavigate();
@@ -14,6 +16,8 @@ const RegisterPhotoPanel = () => {
   const [image, setImage] = useState<HTMLImageElement | null>(null);
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const api = new LS2Request();
+
   const capture = useCallback(() => {
     if (webcamRef.current !== null) {
       const imageSrc = webcamRef.current.getScreenshot();
@@ -28,23 +32,38 @@ const RegisterPhotoPanel = () => {
       ctx?.drawImage(image, 0, 0);
     }
   }, [canvasRef, image]);
-
   const submit = async () => {
     if (image) {
-      console.log({
-        userInfo: userInfo,
-        image: {
-          src: image.src,
-          height: image.height,
-          width: image.width,
-        },
-      });
+      try {
+        // react-query 변경 예정
+        console.log({
+          userInfo: userInfo,
+          image: {
+            src: image.src,
+            height: image.height,
+            width: image.width,
+          },
+        });
+        const option = {
+          service: "com.webos.notification",
+          method: "createToast",
+          parameters: {
+            sourceId: appInfo.id,
+            message: "등록되었습니다",
+          },
+        };
+        api.send(option);
+      } catch (e) {
+        throw new Error("네트워크 오류");
+      }
+    } else {
+      throw new Error("사진을 촬영해주세요");
     }
   };
 
   return (
     <Panel>
-      <Header title="사용자 등록" onClose={() => navigate(-1)} />
+      <Header title="사용자 등록" onClose={() => navigate("/register/info")} />
       <div className={Style.contentWrapper}>
         <Card align_items="flex-start" justify_content="flex_start">
           <h1 className={Style.title}>사용자 정보</h1>
@@ -72,7 +91,16 @@ const RegisterPhotoPanel = () => {
                   await submit();
                   navigate("/");
                 } catch (e) {
-                  console.error("error: ", e);
+                  const option = {
+                    service: "com.webos.notification",
+                    method: "createToast",
+                    parameters: {
+                      sourceId: appInfo.id,
+                      message: (e as Error).message,
+                    },
+                  };
+                  api.send(option);
+                  console.error("error: ", (e as Error).message);
                 }
               }}
             >
