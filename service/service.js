2e7a45644d9b1c2871729ee1755320120fec970a
sijin <skd6150@gmail.com>
2022-11-09 18:45:48 +0900

    :sparkles: heartbeat, recognition service

--- service/service.js
+++ service/service.js
@@ -1,66 +1,15 @@
-/*
- * Copyright (c) 2020-2022 LG Electronics Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// helloworld_webos_service.js
-// is simple service, based on low-level luna-bus API
-
-// eslint-disable-next-line import/no-unresolved
 const pkgInfo = require("./package.json");
 const Service = require("webos-service");
 const spawn = require("child_process").spawn;
 const axios = require("axios");
+const service = new Service(pkgInfo.name);
 
-const service = new Service(pkgInfo.name); // Create service by service name on package.json
 const logHeader = "[" + pkgInfo.name + "]";
 
-service.register("ping", function (message) {
-  axios.post("localhost:3000", { message: "Ping" });
-  console.log(logHeader, "SERVICE_METHOD_CALLED:/ping");
-  console.log("Ping! setting up activity");
-  const methodName = "luna://" + pkgInfo.name + "/pong";
-  const activitySpec = {
-    activity: {
-      name: "ping pong", // this needs to be unique, per service
-      description: "ping and pong", // required
-      type: { foreground: true, persist: false, explicit: true },
-      callback: {
-        // what service to call when this activity starts
-        method: methodName, // URI to service
-      },
-    },
-    start: true, // start the activity immediately when its requirements (if any) are met
-    replace: true, // if an activity with the same name already exists, replace it
-    subscribe: true, // if "subscribe" is false, the activity needs to be adopted immediately, or it gets canceled
-  };
-  service.call("luna://com.webos.service.activitymanager/create", activitySpec, function (reply) {
-    console.log(logHeader, "SERVICE_METHOD_CALLED:com.webos.service.activitymanager/create");
-    console.log("activitySpec: ", activitySpec);
-    const activityId = reply.payload.activityId;
-    console.log("ActivityId = " + activityId);
-    service.call("luna://com.webos.service.activitymanager/getActivityInfo", { activityId: activityId, current: true }, function (activityInfo) {
-      console.log("Acitivity Info: ", activityInfo);
-    });
-    message.respond({ msg: "Created activity " + activityId });
-  });
-});
+let interval;
+let subscription;
 
-service.register("pong", function (message) {
-  console.log(logHeader, "SERVICE_METHOD_CALLED:/pong");
-  console.log("Pong!");
-  console.log(message.payload);
-  axios.post("localhost:3000", { message: "Pong" });
-  message.respond({ message: "Pong" });
-});
-
-service.register("/do/re/me", function (message) {
-  console.log(logHeader, "SERVICE_METHOD_CALLED://do/re/me");
-  message.respond({ verses: [{ doe: "a deer, a female deer" }, { ray: "a drop of golden sun" }, { me: "a name I call myself" }] });
-});
-
-service.register("recognization", async function (message) {
+service.register("recognition", async function (message) {
   const spawnPythonProcess = async () => {
     const pythonProcess = spawn("python3", ["./test.py"]);
     console.log("process start", pythonProcess.pid, process.cwd());
@@ -87,5 +36,65 @@ service.register("recognization", async function (message) {
   } catch (err) {
     console.log(err);
     message.respond({ respond: "error" });
+  }
+});
+
+const heartbeat = service.register("heartbeat");
+
+heartbeat.on("request", function (message) {
+  console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat/request");
+  console.log("heartbeat message: ", message);
+  message.respond({ event: "beat" });
+  if (message.isSubscription) {
+    if (!interval) {
+      createInterval();
+    }
+  }
+});
+
+heartbeat.on("cancel", function (message) {
+  console.log(logHeader, "SERVICE_METHOD_CALLED:/heartbeat/cancel");
+  clearInterval(interval);
+  interval = undefined;
+});
+
+function createInterval() {
+  if (interval) {
+    return;
+  }
+  console.log(logHeader, "create_interval");
+  interval = setInterval(function () {
+    sendResponses();
+  }, 1000);
+}
+
+function sendResponses() {
+  console.log(logHeader, "send_response");
+}
+
+service.register("start", function (message) {
+  subscription = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, { subscribe: true });
+  subscription.addListener("response", function (payload) {
+    console.log("payload: ", payload);
+  });
+  message.respond({
+    returnValue: true,
+    Response: "service has been started.",
+  });
+});
+
+service.register("stop", function (message) {
+  if (subscription === undefined) {
+    message.respond({
+      returnValue: false,
+      Response: "service hasn't been started yet.",
+    });
+  } else {
+    subscription.cancel();
+    subscription = undefined;
+    message.respond({
+      returnValue: true,
+      Response: "service has been stoped.",
+    });
   }
 });
--- service/test.py
+++ service/test.py
@@ -5,7 +5,7 @@ print("before sleep")
 
 cnt = 0
 for i in range(3):
-    time.sleep(0.1)
+    time.sleep(1)
     cnt += 1
 
 print("after sleep. value: {0}".format(cnt))
