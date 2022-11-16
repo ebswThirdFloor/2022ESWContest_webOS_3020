653dee54fa76b9b08b924ff0c6f0e56c8bccf8c6
sijin <skd6150@gmail.com>
2022-11-16 13:20:47 +0900

    :sparkles: record js-service 추가

--- service/service.js
+++ service/service.js
@@ -5,33 +5,41 @@ const fs = require("fs");
 const service = new Service(pkgInfo.name);
 
 const logHeader = "[" + pkgInfo.name + "]";
+const filePath = process.cwd();
+const imagePath = "/tmp/thirdfloor/";
+const intervaltime = 1000;
+const captureParam = {
+  width: 640,
+  height: 480,
+  format: "JPEG",
+  mode: "MODE_ONESHOT",
+};
+const recordNum = 3;
 
 let interval;
 let subscription;
 let cameraId;
 let handle;
-const path = "/tmp/thirdfloor/";
-const intervaltime = 1000;
 
-service.register("recognition", async function (message) {
-  console.log(logHeader, "SERVICE_METHOD_CALLED:/recognition");
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+service.register("identify", async function (message) {
+  console.log(logHeader, "SERVICE_METHOD_CALLED:/identify");
   const spawnPythonProcess = async () => {
     const pythonProcess = spawn("python3", ["./test.py"]);
-    console.log("process start", pythonProcess.pid, process.cwd());
     let data = "";
     for await (const chunk of pythonProcess.stdout) {
-      console.log("stdout: ", chunk);
       data += chunk;
     }
     let error = "";
     for await (const chunk of pythonProcess.stderr) {
-      console.log("stderr: ", chunk);
       data += chunk;
     }
     const exitCode = await new Promise((resolve, reject) => {
       pythonProcess.on("close", resolve);
     });
-    console.log("exitcode: ", exitCode);
     if (exitCode) throw new Error(`error: exit code ${exitCode}, ${error}`);
     return data;
   };
@@ -39,7 +47,7 @@ service.register("recognition", async function (message) {
     const res = await spawnPythonProcess();
     message.respond({ Response: res });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     message.respond({ errorText: "spawnPythonProcess error" });
   }
 });
@@ -65,22 +73,17 @@ heartbeat.on("cancel", function (message) {
 
 function createInterval() {
   if (interval) {
+    console.error("interval undefined");
     return;
   }
   console.log(logHeader, "create_interval");
   interval = setInterval(async function () {
     try {
-      captureParam = {
-        width: 640,
-        height: 480,
-        format: "JPEG",
-        mode: "MODE_ONESHOT",
-      };
       await capture(captureParam);
-      fs.readdir(path, (err, files) => {
+      fs.readdir(imagePath, (err, files) => {
         if (err) console.error(err);
-        console.log(files);
-        files.forEach((file) => fs.unlinkSync(path + file));
+        // console.log(files);
+        files.forEach((file) => fs.unlinkSync(imagePath + file));
       });
     } catch (e) {
       console.error(e);
@@ -88,15 +91,20 @@ function createInterval() {
   }, intervaltime);
 }
 
-async function capture(captureParam) {
+function capture(captureParam) {
   return new Promise((resolve, reject) => {
+    if (!handle)
+      reject({
+        returnValue: false,
+        errorText: "handle value undefined",
+      });
     service.call("luna://com.webos.service.camera2/startPreview", { handle, params: { type: "sharedmemory", source: "0" } }, (startPreviewResponse) => {
       if (!startPreviewResponse.payload.returnValue) {
         reject(startPreviewResponse);
         return;
       } else {
         key = startPreviewResponse.payload.key;
-        service.call("luna://com.webos.service.camera2/startCapture", { handle, params: captureParam, path }, (captureResponse) => {
+        service.call("luna://com.webos.service.camera2/startCapture", { handle, params: captureParam, path: imagePath }, (captureResponse) => {
           if (!captureResponse.payload.returnValue) {
             reject(captureResponse);
             return;
@@ -112,38 +120,114 @@ async function capture(captureParam) {
   });
 }
 
-service.register("start", function (message) {
-  service.call("luna://com.webos.service.camera2/getCameraList", {}, function (cameraListResponse) {
-    const deviceList = cameraListResponse.payload.deviceList;
-    if (deviceList.length === 0 || !cameraListResponse.payload.returnValue) {
-      message.respond({
-        returnValue: true,
-        errorText: "camera not found",
-      });
-      return;
-    } else {
-      cameraId = deviceList[0].id;
-      service.call("luna://com.webos.service.camera2/open", { id: cameraId }, function (openResponse) {
-        if (!openResponse.payload.returnValue) {
-          message.respond({
-            returnValue: false,
-            errorCode: openResponse.payload.errorCode,
-            errorText: openResponse.payload.errorText,
+async function setDir() {
+  const oldmask = process.umask(0);
+  const result = await Promise.all(
+    [filePath + "/users", imagePath].map(
+      (path) =>
+        new Promise((resolve, reject) => {
+          fs.mkdir(path, "1777", (err) => {
+            if (err && err.code != "EEXIST") reject(error);
+            else resolve(null);
           });
-          return;
-        }
-        handle = openResponse.payload.handle;
-        subscription = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, { subscribe: true });
-        message.respond({
-          returnValue: true,
-          Response: "service has been started.",
-        });
-      });
+        })
+    )
+  );
+  process.umask(oldmask);
+  return result;
+}
+
+service.register("record", async function (message) {
+  try {
+    await openCamera();
+    for (var i = 0; i < recordNum; i++) {
+      await capture(captureParam);
+      await sleep(500);
     }
+    await closeCamera();
+    message.respond({
+      returnValue: true,
+    });
+  } catch (e) {
+    console.error(e);
+    message.respond({
+      returnValue: false,
+      errorText: e.errorText,
+    });
+  }
+});
+
+function openCamera() {
+  return new Promise((resolve, reject) => {
+    service.call("luna://com.webos.service.camera2/getCameraList", {}, function (cameraListResponse) {
+      const deviceList = cameraListResponse.payload.deviceList;
+      if (deviceList.length === 0 || !cameraListResponse.payload.returnValue) {
+        reject({
+          returnValue: false,
+          errorText: "camera not found",
+        });
+      } else {
+        cameraId = deviceList[0].id;
+        service.call("luna://com.webos.service.camera2/open", { id: cameraId }, function (openResponse) {
+          if (!openResponse.payload.returnValue) {
+            reject({
+              returnValue: false,
+              errorCode: openResponse.payload.errorCode,
+              errorText: openResponse.payload.errorText,
+            });
+          } else {
+            handle = openResponse.payload.handle;
+            resolve({
+              returnValue: true,
+              Response: `camera handle vaule: ${handle}`,
+            });
+          }
+        });
+      }
+    });
+  });
+}
+
+function closeCamera() {
+  return new Promise((resolve, reject) => {
+    service.call("luna://com.webos.service.camera2/close", { handle }, function (response) {
+      cameraId = handle = undefined;
+      if (response.payload.returnValue) {
+        resolve({
+          returnValue: true,
+          Response: "camera closed",
+        });
+      } else {
+        reject({
+          returnValue: false,
+          errorCode: response.errorCode,
+          errorText: response.errorText,
+        });
+      }
+    });
+  });
+}
+
+service.register("start", async function (message) {
+  try {
+    await setDir();
+  } catch (e) {
+    console.error(e);
+    message.respond({
+      returnValue: false,
+      errorText: "mkdir failed",
+    });
+    return;
+  }
+  await openCamera();
+  subscription = service.subscribe(`luna://${pkgInfo.name}/heartbeat`, { subscribe: true });
+  message.respond({
+    returnValue: true,
+    Response: "service has been started.",
   });
 });
 
-service.register("stop", function (message) {
+service.register("stop", async function (message) {
   if (subscription === undefined) {
     message.respond({
       returnValue: false,
@@ -152,19 +236,6 @@ service.register("stop", function (message) {
   } else {
     subscription.cancel();
     subscription = undefined;
-    service.call("luna://com.webos.service.camera2/close", { handle }, function (response) {
-      if (response.payload.returnValue) {
-        message.respond({
-          returnValue: true,
-          Response: "service has been stoped.",
-        });
-      } else {
-        message.respond({
-          returnValue: false,
-          errorCode: response.errorCode,
-          errorText: response.errorText,
-        });
-      }
-    });
+    await closeCamera();
   }
 });
